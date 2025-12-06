c88bcc18716301d3952673ae4d37c7d1bbe17eed
Desmond Bennett <desbenn@yahoo.com>
2025-12-06 10:37:54 -0500

    General updates.

--- src/main/java/jm/com/dpbennett/jmts/service/ApplicationConfig.java
+++ src/main/java/jm/com/dpbennett/jmts/service/ApplicationConfig.java
@@ -5,29 +5,29 @@
 package jm.com.dpbennett.jmts.service;
 
 import java.util.Set;
-import javax.ws.rs.core.Application;
+//import javax.ws.rs.core.Application;
 
 /**
  *
  * @author Desmond Bennett
  */
-@javax.ws.rs.ApplicationPath("services")
-public class ApplicationConfig extends Application {
-
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
-        return resources;
-    }
-
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
-    private void addRestResourceClasses(Set<Class<?>> resources) {
-    }
-    
-}
+//@javax.ws.rs.ApplicationPath("services")
+//public class ApplicationConfig extends Application {
+//
+//    @Override
+//    public Set<Class<?>> getClasses() {
+//        Set<Class<?>> resources = new java.util.HashSet<>();
+//        addRestResourceClasses(resources);
+//        return resources;
+//    }
+//
+//    /**
+//     * Do not modify addRestResourceClasses() method.
+//     * It is automatically populated with
+//     * all resources defined in the project.
+//     * If required, comment out calling this method in getClasses().
+//     */
+//    private void addRestResourceClasses(Set<Class<?>> resources) {
+//    }
+//    
+//}
